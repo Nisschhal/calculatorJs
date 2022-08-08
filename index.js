aeb0214d3d1a0e302fa3b9c4b053d379c2af5137
Nisschhal <mrnischalpuri@gmail.com>
2022-08-08 09:59:02 +0545

    simpole calculator with js eval() compelete

--- index.js
+++ index.js
@@ -28,33 +28,50 @@
 
 // };
 
-function print() {
-    // var opt1 = document.getElementById("opt1");
-    // var opt2 = document.getElementById("opt2");
+// function print() {
+//     var opt1 = document.getElementById("opt1");
+//     var opt2 = document.getElementById("opt2");
 
-    // if (opt1.checked) {
-    //     alert(`Option 1 is clicked!! ${opt1.value}`)
-    // }
-    // else if (opt2.checked) {
-    //     alert(`Option 2 is clicked!! ${opt2.value}`)
-    // } else {
-    //     alert("Nothing is clicked!!")
-    // }
+//     if (opt1.checked) {
+//         alert(`Option 1 is clicked!! ${opt1.value}`)
+//     }
+//     else if (opt2.checked) {
+//         alert(`Option 2 is clicked!! ${opt2.value}`)
+//     } else {
+//         alert("Nothing is clicked!!")
+//     }
 
-    var select = document.getElementById("s");
-    console.log(select);
-    console.log(select.options[select.selectedIndex].value);
+//     var select = document.getElementById("s");
+//     console.log(select);
+//     console.log(select.options[select.selectedIndex].value);
     
-    if (select.options[select.selectedIndex].value == 'One'){
-        alert("The value is one")
-    }
-    else if (select.options[select.selectedIndex].value.toLowerCase() == 'two'){
-        alert("The value is two")
-    }
-    else if (select.options[select.selectedIndex].value == 'Three'){
-        alert("The value is three")
-    }
+//     if (select.options[select.selectedIndex].value == 'One'){
+//         alert("The value is one")
+//     }
+//     else if (select.options[select.selectedIndex].value.toLowerCase() == 'two'){
+//         alert("The value is two")
+//     }
+//     else if (select.options[select.selectedIndex].value == 'Three'){
+//         alert("The value is three")
+//     }
 
+// }
+
+
+var display = (val) => {
+    document.getElementById("display").value += val;
+}
+
+
+
+var equal = () => {
+    var x = document.getElementById("display").value;
+    var y = eval(x);
+    document.getElementById("display").value = y;
+}
+
+var clr = () => {
+    document.getElementById("display").value = " ";
 }
 
 
